524103faa7432a91134558c14fa796cf15b285f9
KeitaFukue <k.fukue.it@gmail.com>
2021-05-01 00:52:51 +0900

    firestoreでDBからデータ取得

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,37 @@
 import 'package:coriander_app/main_model.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'book_list_page.dart';
 
-void main() {
+Future <void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
 
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          print("${snapshot.error}");
+          return Container();
+        }
+        if (snapshot.connectionState == ConnectionState.done) {
+          return Main();
+        }
+        return Container();
+      },
+    );
+  }
+}
+
+class Main extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -35,6 +59,10 @@ class MyApp extends StatelessWidget {
                       child:Text('ボタン'),
                       onPressed: (){
                         model.changeTempText();
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => BookList()),
+                        );
                       },
                     ),
                   ],
