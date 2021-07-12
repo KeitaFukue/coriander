31d6f1cae87fe258f34a449706adae0af1aec81c
KeitaFukue <k.fukue.it@gmail.com>
2021-07-12 20:18:48 +0900

    本一覧ページでの削除を，リストの長押しから，アイコンボタンに変更

--- lib/presentation/book_list/book_list_page.dart
+++ lib/presentation/book_list/book_list_page.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:coriander_app/presentation/add_book/add_book_page.dart';
 import 'book_list_model.dart';
 import 'package:flutter/material.dart';
@@ -8,11 +7,8 @@ import 'package:provider/provider.dart';
 class BookListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    FirebaseFirestore firestore = FirebaseFirestore.instance;
 
     return ChangeNotifierProvider<BookListModel>(
-      create: (_) => BookListModel()..fetchBooks(),
-
       child: Scaffold(
         appBar: AppBar(
           title: Text('本一覧'),
@@ -27,40 +23,45 @@ class BookListPage extends StatelessWidget {
                 leading: book.imageURL != null
                     ?Image.network(book.imageURL)
                     :Image.network('https://1.bp.blogspot.com/-D2I7Z7-HLGU/Xlyf7OYUi8I/AAAAAAABXq4/jZ0035aDGiE5dP3WiYhlSqhhMgGy8p7zACNcBGAsYHQ/s1600/no_image_square.jpg'),
-                trailing: IconButton(
-                  icon: Icon(Icons.create),
-                  onPressed: ()async{//本一覧ページから本編集ページに遷移
-                    await Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => AddBookPage(book: book,),//BookListModelに入っているListであるbooksの各要素がbook
-                        fullscreenDialog: true,//画面遷移のモーションが変わる
-                      ),
-                    );
-                    model.fetchBooks();//本編集ページから本一覧ページに戻ってきた時に、firestoreの値を自動取得
-                  },
-                ),
-
-                onLongPress: ()async{
-                  await showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                    return AlertDialog(
-                      title: Text('削除しますか？'),
-                      actions: <Widget>[
-                        TextButton(
-                          child: Text('OK'),
-                          onPressed: () {
-                            model.deleteBook(book);
-                            Navigator.of(context).pop();//Dialogを消して本追加のページに戻る
-                            model.fetchBooks();
+                trailing: Wrap(
+                    children: <Widget>[
+                      IconButton(
+                        icon: Icon(Icons.create),
+                        onPressed: ()async{
+                          //本一覧ページから本編集ページに遷移
+                          await Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => AddBookPage(book: book,),//BookListModelに入っているListであるbooksの各要素がbookfullscreenDialog: true,//画面遷移のモーションが変わる
+                            ),
+                          );
+                          model.fetchBooks();//本編集ページから本一覧ページに戻ってきた時に、firestoreの値を自動取得
                           },
-                        ),
-                      ],
-                    );
-                  },
-                  );
-                },
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.delete),
+                        onPressed: ()async{
+                          await showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: Text('削除しますか？'),
+                                actions: <Widget>[
+                                  TextButton(
+                                    child: Text('OK'),
+                                    onPressed: () {
+                                      model.deleteBook(book);
+                                      Navigator.of(context).pop();//Dialogを消して本追加のページに戻る
+                                      model.fetchBooks();
+                                    },
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                        },
+                      )
+                    ])
               ),
             ).toList();
 
@@ -87,6 +88,8 @@ class BookListPage extends StatelessWidget {
             }
         ),
       ),
+
+      create: (_) => BookListModel()..fetchBooks(),
     );
   }
 }
